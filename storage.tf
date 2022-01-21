9ecb85dd949855daa3462f9a8f004779b5bfe832
hvtung <hvtung@tma.com.vn>
2022-01-21 07:32:19 +0000

    Inital

---
+++ storage.tf
@@ -0,0 +1,38 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+  backend "local" {
+    path = "backend/terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Southeast Asia"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "projektordefaultstorage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
